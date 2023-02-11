72b410cd3955a7a419df58a01304fda9ea571c42
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-02-11 17:46:24 +0530

    setting up folder structure and express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error occured : ${err}`);
+    }
+
+    console.log(`Server is running on port ${port}`)
+})
