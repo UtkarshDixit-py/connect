15fb4f02ed12b28bfb8eda8cbe14076f0e651b91
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-02-11 21:03:51 +0530

    set up view engine

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const homeController = require('../controllers/home_controller');
 
 
 router.get('/',homeController.home)
+router.use('/users',require('./users'))
+
 
 module.exports = router;console.log("router file is loaded")
