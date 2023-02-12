d399d1347c23bda7dae4fd7b57a6524729672e4a
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-02-12 21:19:52 +0530

    static files

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const expressLayouts = require("express-ejs-layouts")
 const app = express();
 const port = 8000;
 
+app.use(express.static('./assets'))
+
 app.use(expressLayouts)
+app.set('layout extractStyles',true);
+app.set('layout extractScript',true);
+
 
 //use express router
 app.use('/', require('./routes'))
