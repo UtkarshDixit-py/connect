d8da8a912844c19c2df84ab92a4bfe7bb570edec
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-02-24 12:09:26 +0530

    setting up scss

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const session = require('express-session');
 const passport = require('passport')
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
 
 const cookieParser = require('cookie-parser');
 const db = require('./config/mongoose');
 const app = express();
 const port = 8000;
 
+
+app.use(sassMiddleware({
+  
+    src: '/assets/scss',
+    dest: 'assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}))
 app.use(express.urlencoded());
 
 app.use(cookieParser());
